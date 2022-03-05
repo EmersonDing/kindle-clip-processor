4fccbc1af2e98039c92c3521d65afe604b088fbf
Nan Ding <emerson@Nans-Mac-mini.local>
2022-03-05 23:58:08 +0800

    Refactor kindle processor methods

--- kindle_clipper_processor.py
+++ kindle_clipper_processor.py
@@ -1,9 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import datetime
+import re
 
 input_path = 'resource/clip.txt'
 output_path = 'resource/output.txt'
 chunk_delimiter = '=========='
+
+
+class Chunk:
+    def __init__(self, raw_chunk):
+        self.title, self.metadata, self.text = self.normalize_chunk(raw_chunk)
+        self.date = self.extract_date()
+
+    def normalize_chunk(self, raw_chunk):
+        """
+        Chunk is combined with title | metadata | paragraph, e.g.:
+        ```
+            舞!舞!舞!
+            - 您在位置 #133-134的标注 | 添加于 2015年9月24日星期四 上午5:08:43
+            我这人决没有什么不正常。 我的确如此认为。
+        ```
+
+        Normalize includes:
+        * filter empty lines.
+        * transform string to list.
+        """
+        lines = raw_chunk.split('\n')
+        chunk = list(filter(lambda line: line != '', lines))
+        # Some clips have email addr in title, e.g. "title (email)"
+        title = chunk[0].replace('(bingshuishenshi@126.com)', '')
+        metadata = chunk[1]
+        text = '\n'.join(chunk[2:])
+
+        # for i in range(0, len(chunk)):
+        #     print('line {0}: <{1}>'.format(i, chunk[i]))
+        #
+        # self.metadata = metadata
+        # print(self.extract_date())
+
+        return title, metadata, text
+
+    def extract_date(self):
+        """
+        Extract date from metadata.
+        Metadata sample: "- 您在位置 #133-134的标注 | 添加于 2015年9月24日星期四 上午5:08:43".
+        """
+        date_regex = re.search(r"(\d+)年(\d+)月(\d+)日", self.metadata)
+        return datetime.date(int(date_regex.group(1)), int(date_regex.group(2)), int(date_regex.group(3)))
+
 
 
 def get_file():
@@ -18,7 +63,7 @@ def get_file_string(f):
     return s
 
 
-def get_raw_chunk(s):
+def get_raw_chunks(s):
     """
     Chunk is combined with title | metadata | paragraph, e.g.:
     ```
@@ -27,7 +72,8 @@ def get_raw_chunk(s):
         我这人决没有什么不正常。 我的确如此认为。
     ```
     """
-    return s.split(chunk_delimiter)
+    raw_chunks = s.split(chunk_delimiter)
+    return strip_empty_strings(raw_chunks)
 
 
 def normalize_chunks(chunks):
@@ -38,7 +84,7 @@ def normalize_chunks(chunks):
     new_chunks = []
     for chunk in chunks:
         lines = chunk.split('\n')
-        new_chunk = list(filter(lambda line: line != '', lines))
+        new_chunk = strip_empty_strings(lines)
         new_chunks.append(new_chunk)
     return new_chunks
 
@@ -46,13 +92,11 @@ def normalize_chunks(chunks):
 def classify_chunks(chunks):
     dict = {}
     for chunk in chunks:
-        if len(chunk) <= 2:
+        if chunk.text is None:
             continue
-        # Some clips have email addr in title, e.g. "title (email)"
-        title = chunk[0].replace('(bingshuishenshi@126.com)', '')
-        if dict.get(title) is None:
-            dict[title] = []
-        dict[title].append(chunk[2])
+        if dict.get(chunk.title) is None:
+            dict[chunk.title] = []
+        dict[chunk.title].append(chunk.text)
     return dict
 
 
@@ -99,13 +143,16 @@ def output_file(chunk_dict):
     f.close()
 
 
+def strip_empty_strings(strings):
+    return list(filter(lambda s: s != '' and s != '\n', strings))
+
+
 if __name__ == '__main__':
     f = get_file()
     s = get_file_string(f)
-    raw_chunks = get_raw_chunk(s)
-    normalized_chunks = normalize_chunks(raw_chunks)
-    test_chunks(normalized_chunks)
-    chunk_dict = classify_chunks(normalized_chunks)
+    raw_chunks = get_raw_chunks(s)
+    chunks = list(map(lambda c: Chunk(c), raw_chunks))
+    chunk_dict = classify_chunks(chunks)
     print_titles(chunk_dict)
     print_chunk_dict(chunk_dict)
     output_file(chunk_dict)
